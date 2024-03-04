0db41ec2b98c66341ffa85334750b3a5c1069289
ksharugeshwaran <sharugeshwaran.k@kalvium.community>
2024-03-04 13:01:54 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,23 @@
-import './App.css';
-import elephant from "./images/elephant.jpeg";
-import React from "react"
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
+import React from "react";
+import "./App.css";
 
-
-function App() {
-  const img_Arr = imageData()
+function App({ imageData }) {
   return (
-    <>
-      <h1>Kalvium Gallery</h1>
-      <div className='grid'>
-        {/* <img src={img_Arr[0].img} alt="" className='photo'/>    
-        <img src={img_Arr[1].img} alt="" className='photo'/>    
-        <img src={img_Arr[2].img} alt="" className='photo'/>    
-        <img src={img_Arr[3].img} alt="" className='photo'/>     */}
-        {
-          img_Arr.map(elem => {
-            return(
-              <img src={elem.img} alt="elephant" className='photo'/>
-            )
-          })
-        }
+    <div>
+      <h2>Kalvium Gallery</h2>
+      <div id="gallery">
+        {imageData.map((e) => (
+          <img
+            className="image"
+            src={e.img}
+            id={e.id}
+            key={e.id}
+            alt={`Elephant ${e.id}`}
+          />
+        ))}
       </div>
-      {
-        
-      }
-    </>
-  )
+    </div>
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,24 @@
-import React,{ Component } from "react";
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
+export default class AppClass extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Kalvium Gallery</h2>
+        <div id="gallery">
+          {this.props.imageData.map((e) => (
+            <img
+              className="image"
+              src={elephant}
+              id={e.id}
+              key={e.id}
+              alt={`Elephant ${e.id}`}
+            />
+          ))}
+        </div>
+      </div>
+    );
   }
-
-  // code here
 }
